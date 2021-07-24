c623d350c76ec8e1428b4fad056d3aabd4efae2f
Nishan <nishan@Nishans-MacBook-Pro.local>
2021-07-24 15:56:14 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const guitarFrequencyMap = {
+  E: 82.41,
+  A: 110,
+  D: 146.83,
+  G: 196,
+  B: 246.94,
+  e: 329.63,
+};
+
+const notes = ["E", "A", "D", "G", "B", "e"];
+class Oscillator {
+  constructor() {
+    this.initializeOscillator();
+  }
+
+  initializeOscillator() {
+    const audioCtx = new window.AudioContext();
+    this.oscillator = audioCtx.createOscillator();
+    this.oscillator.type = "sawtooth";
+    this.oscillator.connect(audioCtx.destination);
+  }
+
+  playNote(frequency) {
+    this.initializeOscillator();
+    this.oscillator.frequency.value = frequency;
+    this.oscillator.start();
+  }
+
+  incrementOctave() {
+    this.setFrequency(2 * this.oscillator.frequency.value);
+  }
+
+  decrementOctave() {
+    this.setFrequency(this.oscillator.frequency.value / 2);
+  }
+
+  setFrequency(frequency) {
+    this.oscillator.frequency.value = frequency;
+  }
+
+  stop() {
+    this.oscillator.stop();
+  }
+}
 
 function App() {
+  const oscillator = React.useRef(new Oscillator()).current;
+  const [currentNote, setCurrentNote] = React.useState(null);
+  const [playing, setPlaying] = React.useState(false);
+
+  const handleStop = () => {
+    oscillator.stop();
+    setPlaying(false);
+  };
+
+  const handlePlayNote = (note) => () => {
+    if (playing) {
+      oscillator.setFrequency(guitarFrequencyMap[note]);
+    } else {
+      oscillator.playNote(guitarFrequencyMap[note]);
+    }
+    setCurrentNote(note);
+    setPlaying(true);
+  };
+
+  const incrementOctave = () => {
+    oscillator.incrementOctave();
+  };
+
+  const decrementOctave = () => {
+    oscillator.decrementOctave();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Guitar Ear Tuner</h1>
+      <br />
+      <br />
+      <button className="btn btn-secondary" onClick={incrementOctave}>
+        Increment Octave
+      </button>
+      <button className="btn btn-secondary" onClick={decrementOctave}>
+        Decrement Octave
+      </button>
+      <br />
+      <br />
+      <hr />
+      {notes.map((note) => {
+        return note === currentNote && playing ? (
+          <button className="btn" key={note} onClick={handleStop}>
+            Stop
+          </button>
+        ) : (
+          <button className="btn" key={note} onClick={handlePlayNote(note)}>
+            Play note {note}
+          </button>
+        );
+      })}
     </div>
   );
 }
